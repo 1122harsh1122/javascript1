906875f269bcf9225ef15c47681465ec792450e6
HARSH BIND <69873503+1122harsh1122@users.noreply.github.com>
2023-09-25 18:04:51 +0000

    datatypes

---
+++ o1basics/02variables.js
@@ -0,0 +1,22 @@
+"use strict"; //all the code is treated as newer version
+
+//alert(3 + 3)  we are using node js not browser
+
+console.log( 3+ 3)
+console.log("harsh")
+
+
+let name = "harsh"
+let age = 18
+let isLogggedIn = true
+
+// number => 2 to pwr 53 
+// Bigint
+// string       "" 
+// Boolean = True, False
+// null
+// undefined
+// symbol => unique     whenever we talk about unique we us symbol
+
+console.log(typeof "harsh")
+console.log(typeof null)  // object
